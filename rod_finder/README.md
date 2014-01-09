67d063f2ccd4408011d1fd01ae17f3266d9b4ea7
aleimba <aleimba@gmx.de>
2014-01-09 15:53:29 +0100

    rod_finder v0.3, plus rod_finder's README with changelog

---
+++ rod_finder/README.md
@@ -0,0 +1,124 @@
+rod_finder
+==========
+
+A script to find regions of difference (RODs) between a query genome and reference genome(s).
+
+## Synopsis
+
+    blast_rod_finder_legacy.sh subject.fasta query.fasta query.[embl|gbk|fasta] 5000
+
+or
+
+    perl blast_rod_finder.pl -q query.embl -r blastn.out -m 2000
+
+## Description
+
+This script is intended to identify RODs between a nucleotide query and a nucleotide subject/reference sequence. In order to do so, a *blastn* (http://blast.ncbi.nlm.nih.gov/Blast.cgi) needs to be performed beforehand with the query and the subject sequences (see also *blast_rod_finder_legacy.sh* below). *blast_rod_finder.pl* is mainly designed to work with bacterial genomes, while a query genome can be blasted against several subject sequences to detect RODs over a number of references. Although the results are optimized towards a complete query genome, both the reference(s) as well as the query can be used in draft form. To create artificial genomes use *cat_seq.pl* or the EMBOSS application union (http://emboss.sourceforge.net/).
+
+The *blastn* report file, the query sequence file (preferably in RichSeq format, see below) and a minimum size for ROD detection have to be provided. Subsequently, RODs are summarized in a tab-separated summary file, a gff3 (usable e.g. in Artemis/DNAPlotter, http://www.sanger.ac.uk/resources/software/artemis/) and a BRIG (BLAST Ring Image Generator, http://brig.sourceforge.net/) output file. Nucleotide sequences of each ROD are written to a multi-fasta file.
+
+The query sequence can be provided in RichSeq format (embl or genbank), but has to correspond to the fasta file used in querying the BLAST database (the accession numbers have to correspond to the fasta headers). Use *seq_format-converter.pl* to create a corresponding fasta file from embl|genbank files for *blastn* if needed. With RichSeq query files additional info is given in the result summary and the amino acid sequences of all non-pseudo CDSs, which are contained or overlap a ROD, are written to a result file. Furthermore, all detected RODs are saved in individual sequence files in the corresponding query sequence format.
+
+Run *blastn* and the script *blast_rod_finder.pl* consecutively manually or use the bash shell wrapper script *blast_rod_finder_legacy.sh* (see usage below) to perform the pipeline with one command. The same folder has to contain the subject fasta file(s), the query fasta file, optionally the query RichSeq file and the script *blast_rod_finder.pl*! *blastn* is run **without** filtering of query sequences ('-F F') and an evalue cutoff of '2e-11' is set.
+
+## Usage
+
+### 1.) Manual consecutively
+
+#### 1.1.) *blastn*
+
+    formatdb -p F -i subject.fasta -n blast_db
+    blastall -p blastn -d blast_db -i query.fasta -o blastn.out -e 2e-11 -F F
+
+#### 1.2.)* blast_rod_finder.pl*
+
+    perl blast_rod_finder.pl -q query.[embl|gbk|fasta] -r blastn.out -m 5000
+
+### 2.) With one command: *blast_rod_finder_legacy.sh* pipeline
+
+    blast_rod_finder_legacy.sh subject.fasta query.fasta query.[embl|gbk|fasta] 5000
+
+## Options for *blast_rod_finder.pl*
+
+### Mandatory options
+
+* -m, -min
+
+Minimum size of RODs that are reported
+
+* -q, -query
+
+Query sequence file [fasta, embl, or genbank format]
+
+* -r, -report
+
+*blastn* report/output file
+
+### Optional options
+
+* -h, -help:   Help (perldoc POD)
+
+## Output
+
+### a.) *blast_rod_finder_legacy.sh* or *blastn*
+
+* *blastn* database files for subject sequence(s)
+
+\*.nhr, \*.nin, \*.nsq
+
+* *blastn* report
+
+Text file named 'blastn.out'
+
+### b.) *blast_rod_finder.pl*
+
+* ./results
+
+All output files are stored in this result folder
+
+* rod_summary.txt
+
+Summary of detected ROD regions (for embl/genbank queries includes annotation), tab-separated
+
+* rod.gff
+
+GFF3 file with ROD coordinates to use in Artemis/DNAPlotter etc.
+
+* rod_BRIG.txt
+
+ROD coordinates to use in BRIG (BLAST Ring Image Generator), tab-separated
+
+* rod_seq.fasta
+
+Nucleotide sequences of ROD regions (>ROD# size start..stop), multi-fasta
+
+* rod_aa_fasta.txt
+
+Only present if query is in RichSeq format. Amino acid sequences of all CDSs that are contained in or overlap a ROD region in multi-fasta format (>locus_tag gene product). RODs are seperated in the file via '\~\~' (\~\~ROD# size start..stop).
+
+* ROD#.[embl|gbk]
+
+Only present if query is in RichSeq format. Each identified ROD is written to an individual sequence file (in the same format as the query).
+
+## Run environment
+
+The Perl script runs under Windows and UNIX flavors, the bash-shell script of course only under UNIX.
+
+## Dependencies
+
+* Legacy blast (tested version blastall 2.2.18)
+* BioPerl (tested with version 1.006901)
+
+## Authors/contact
+
+Andreas Leimbach (aleimba[at]gmx[dot]de; Microbial Genome Plasticity, Institute of Hygiene, University of Muenster)
+
+David Studholme (original code; D[dot]J[dot]Studholme[at]exeter[dot]ac[dot]uk; University of Exeter) 
+
+## Changelog
+
+* v0.3 (23.11.2011)
+    - status messages with autoflush
+    - BRIG output file
+    - extended primary tag output for RODs (in addition to CDS): tRNA, rRNA, tmRNA, ncRNA, misc_RNA, repeat_region, misc_binding, and mobile_element
+* v0.1 (07.11.2011)
--- rod_finder/blast_rod_finder.pl
+++ rod_finder/blast_rod_finder.pl
@@ -6,11 +6,10 @@ use Bio::SeqIO; # bioperl module to handle sequence input/output
 use Bio::SearchIO; # bioperl module to handle blast reports
 use Bio::Seq; # bioperl module to handle sequences with features
 use Bio::SeqFeatureI; # bioperl module to handle features in a sequence (only possible for rich sequence formats, like embl/genbank, not fasta)
-# use CGI; # relic from the html print out at the bottom of the script, old David Studholme code
 
 my $usage = "\n".
    "\t#####################################################################\n".
-   "\t# $0 blastn_report query-seq_file minimum_ROD_size #\n". #$0 = program name
+   "\t# $0 blastn_report query-seqfile min_ROD_size      #\n". #$0 = program name
    "\t#                                                                   #\n".
    "\t# Searches for regions of difference (RODs) in a query sequence     #\n".
    "\t# according to a blastn run. Give the BLAST query sequence file in  #\n".
@@ -19,12 +18,13 @@ my $usage = "\n".
    "\t# minimum_ROD_size = minimum size of detected RODs (in bp)          #\n".
    "\t# The perl script uses bioperl (www.bioperl.org).                   #\n".
    "\t#                                                                   #\n".
-   "\t# version 0.2, update: 17.11.2011  Andreas Leimbach/David Studholme #\n".
-   "\t# 07.11.2011                      andreas.leimbach\@uni-wuerzburg.de #\n".
+   "\t# version 0.3, update: 23.11.2011  Andreas Leimbach/David Studholme #\n".
+   "\t# 07.11.2011                                  aleimba[at]gmx[dot]de #\n".
    "\t#####################################################################\n\n";
 
 ### ToDo:
 # - 'blasttable' (more efficient runtime) and 'blastxml' (accepts standard blast report format changes) blastreport formats don't work. Might depend on bioperl version. --> possible to check blast report format?
+
 
 ### Print usage if -h|--h|--help is given as argument or arguments are not given
 my ($blast_report, $q_file, $minimum_size) = @ARGV; # minimum size of uncovered regions that are reported
@@ -35,11 +35,14 @@ if (!defined($blast_report) || !defined($q_file)) {
 }
 
 
+
 ### Parse the blast report/output file
 my %q_covered; # hash which stores for each position (=hash-key) of the query sequence if it falls within a hsp hit
 my @blast_q_acc; # test if identical query accessions in blast parse and seqio (below)
 my $blast_db;
 my $parser = new Bio::SearchIO(-file => "<$blast_report", -format => 'blast'); # Bio::SearchIO object
+$|++; # turn on autoflush, forces STDOUT flush right away (STDOUT normally line-/block-buffered)
+print "\nParsing blast report file."; # status display (with autoflush)
 while(my $result = $parser->next_result) { # several query sequences possible (result = entire analysis for a single query seq!) -> e.g. usable with multi-fasta query file
     my $query_acc = $result->query_accession;
     $query_acc =~ s/\.$//; # somehow with contig ID-lines '->query_accession' appends a '.'
@@ -48,6 +51,7 @@ while(my $result = $parser->next_result) { # several query sequences possible (r
     $blast_db = $result->database_name; # For use in the GFF3 print out
     chop $blast_db; # always an empty character after the name?
     while(my $hit = $result->next_hit) { # several subject sequences in the database might have hits
+	print '.'; # status display
 	while(my $hsp = $hit->next_hsp) { # each hit might have one or more hsps (the alignments shown in a blast report)
 	    my $hit_start = $hsp->start('hit'); # reference/subject hsp start coordinate
 	    my $hit_end = $hsp->end('hit');
@@ -66,7 +70,9 @@ my %features; # hash which stores all features for RichSeq files
 my $multi = 0; # test if it is a multi sequence query file (multi-fasta/embl/genbank ...) -> $multi >= 2
 my $i = 0; # needed to test for identical acc.-nr.s
 my $seqio_obj = Bio::SeqIO->new(-file => "<$q_file"); # didn't use '-format' to leave it to bioperl guessing
+print "\nParsing query sequence file."; # status display (with autoflush)
 while (my $seq_obj = $seqio_obj->next_seq) { # Bio::Seq object, query might be multi-fasta/embl/genbank
+    print '.'; # status display
     my $query_acc; # fits to $query_acc above in blast-result parse
     if (ref($seqio_obj) =~ m/\:\:fasta$/i) { # $seqio_obj blessed, thus ref returns package name (here: Bio::SeqIO::fasta)
 	$query_acc = $seq_obj->display_id; # Bio::Seq method, '->accession_number' doesn't work with fasta files
@@ -87,7 +93,9 @@ while (my $seq_obj = $seqio_obj->next_seq) { # Bio::Seq object, query might be m
 
 ### Get the contiguous unaligned regions of query seqs
 my %uncovered_regions; # hash to store the uncovered regions
+print "\nLooking for RODs."; # status display (with autoflush)
 foreach my $acc (keys %q_seqobj) {
+    print "."; # status display
     my $start;
     my $seq = $q_seqobj{$acc}->seq; # Bio::SeqIO object method 'seq'
     my $previous_state = 1;
@@ -121,21 +129,27 @@ my $seq_out = 'rod_seq.fasta'; # Multi-fasta file of the ROD sequences
 open(SEQ, ">$seq_out") or die "Failed to create $seq_out file: $!\n";
 my $gff_out = 'rod_result.gff'; # GFF3 file of ROD regions for use in artemis/DNAPlotter etc.
 open(GFF, ">$gff_out") or die "Failed to create $gff_out file: $!\n";
+my $brig_out = 'rod_BRIG.txt'; # tab-seperated file to load into BRIG
+open (BRIG, ">$brig_out") or die "Failed to create $brig_out file: $!\n";
 print ROD "$q_file regions with no blast hits (RODs) in $blast_report\n";
 print ROD "Rank\tLength\tROD position";
+my $cds_out;
 if (!(ref($seqio_obj) =~ m/\:\:fasta$/i)) { # if not a fasta file as query (see above)
     print ROD "\tROD CDSs\tCDSs position";
-    my $cds_out = 'rod_aa_fasta.txt'; # CDS amino acid sequence output file only makes sense if query file not a fasta file
+    $cds_out = 'rod_aa_fasta.txt'; # CDS amino acid sequence output file only makes sense if query file not a fasta file
     open(CDS, ">$cds_out") or die "Failed to create $cds_out file: $!\n";
 }
 print ROD "\n";
 print GFF "##gff-version 3\n"; # header, shows it's a gff version 3 file
 print GFF "#$q_file regions with no blast hits (RODs) in $blast_report\n"; # comment line for description
+print BRIG "#Start\tStop\tLabel\n"; # comment line as column headers
 
+print "\nPreparing results."; # status display (with autoflush)
 my $rank = 0; # number of ROD regions
 #my $minimum_size = 100; # minimum size of uncovered regions that are reported
 foreach my $length (sort {$b<=>$a} keys %uncovered_regions) { # sort from large length to small length
     if ($length >= $minimum_size) {
+	print "."; # status display
 	foreach my $acc (sort keys %{$uncovered_regions{$length}}) {
 	    foreach my $start (sort @{$uncovered_regions{$length}{$acc}}) {
 		my $end = $start + $length - 1;
@@ -147,6 +161,7 @@ foreach my $length (sort {$b<=>$a} keys %uncovered_regions) { # sort from large 
 		    $pos = "$acc: $start..$end";
 		}
 		print GFF "$acc\tBLASTN\tsequence_difference\t$start\t$end\t.\t+\t.\tName=ROD$rank;Target=$blast_db;color=2\n"; ### Include ...$rank;Target= $hit_acc $hit_start $hit_stop;color ...?
+		print BRIG "$start\t$end\tROD$rank\n";
 		print ROD "$rank\t$length\t$pos";
  		print SEQ ">ROD$rank\_$length\_$pos\n";
 		print SEQ $q_seqobj{$acc}->subseq($start,$end), "\n\n"; # subseq method of Bio::Seq object
@@ -157,13 +172,19 @@ foreach my $length (sort {$b<=>$a} keys %uncovered_regions) { # sort from large 
 			print CDS "\n";
 		    }
 		    my $loop = 0;
+		    my $features_exist = 0; # Are features in the ROD? If not print "\n"!
 		    print CDS "--ROD$rank\_$length\_$pos\n";
 		    foreach my $feature (@{$features{$acc}}) {
 			if ($feature->location->start >= $start && $feature->location->end <= $end) { # features that are fully within ROD region
+			    $features_exist = 1;
 			    $loop = print_CDSs($feature, $loop); # subroutine
 			} elsif (($feature->location->start <= $start && ($feature->location->end > $start && $feature->location->end <= $end)) || (($feature->location->start >= $start && $feature->location->start < $end) && $feature->location->end > $end)) { # features that overlap the ROD region, either only with their end or start coordinates
+			    $features_exist = 1;
 			    $loop = print_CDSs($feature, $loop, 1);
 			}
+		    }
+		    if ($features_exist == 0) { # Print "\n" if no features exist in the ROD
+			print ROD "\n";
 		    }
                     ### Print out each ROD in the query sequence fileformat ==> but didn't work well!
 #		    my $outfile = "ROD$rank.txt";
@@ -179,124 +200,23 @@ foreach my $length (sort {$b<=>$a} keys %uncovered_regions) { # sort from large 
 close ROD;
 close SEQ;
 close GFF;
+close BRIG;
 if (!(ref($seqio_obj) =~ m/\:\:fasta$/i)) {
     close CDS;
 }
+$| = 0; # turn of autoflush
 
-
-##### Old code for printing out the result in a html file! '#####' = marks old code from David Studholme
-### List the longest uncovered regions and print in html-file
-# my $rod_result = 'rod_result.html';
-# open(ROD, ">$rod_result") or die "Failed to create $rod_result file: $!\n";
-# my $cgi = new CGI;
-# print ROD $cgi->start_html(-title => "$q_file regions with no blast hits (RODs) in $blast_report");
-
-# ##### print $cgi->start_html(-title => "$gbrowse_name: $q_file regions with no blast hits in $blast_report");
-# ##### print "<link rel=\"stylesheet\" href=\"http://www.sainsbury-laboratory.ac.uk/sl.css\">\n";
-
-# print ROD "\n<h3>$q_file regions with no blast hits (RODs) in $blast_report</h3>\n";
-
-# ##### print "\n<h3>$gbrowse_name: $q_file regions with no blast hits in $blast_report</h3>\n";
-
-# print ROD "\n<table border=1>\n";
-# print ROD "<tr>";
-# print ROD "<th>Rank</th>";
-# print ROD "<th>Length</th>";
-# print ROD "<th>ROD Position</th>";
-# if (!(ref($seqio_obj) =~ m/\:\:fasta$/i)) { # see above
-#     print ROD "<th>ROD CDSs</th>";
-#     print ROD "<th>CDSs start:stop</th>";
-#     print ROD "<th>Feature aa sequence</th>"; # Amino acid sequence
-# }
-# print ROD "</tr>";
-# my $i = 0;
-# foreach my $length (sort {$b<=>$a} keys %uncovered_regions) { # sort from large length to small length
-#     if ($length >= $minimum_size) {
-# 	foreach my $acc (sort keys %{$uncovered_regions{$length}}) {
-# 	    foreach my $start (@{$uncovered_regions{$length}{$acc}}) {
-# 		my $end = $start + $length - 1; ### why length-1 again?
-# 		my $pos;
-# 		if ($multi <= 1) { # accession# only included if the query is a multi-sequence file
-# 		    $pos = "$start..$end";
-# 		} elsif ($multi >= 2) {
-# 		    $pos = "$acc: $start..$end";
-# 		}
-
-# #####		my $url="http://144.173.144.7/cgi-bin/gb2/gbrowse/$gbrowse_name?name=$pos";
-# #####		#my $url="http://jic55119.jic.bbsrc.ac.uk/cgi-bin/gbrowse/$gbrowse_name?name=$pos";
-# #####		my @CDS = get_CDSs($pos, $gbrowse_name);
-
-# 		$i++;
-# 		print ROD "<tr>";
-# 		print ROD "<td>$i</td>";
-# 		print ROD "<td>$length</td>";
-# 		print ROD "<td>$pos</td>";
-
-# # Include a link here to the sequences of the ROD included genes, i.e. print them out in another html file (instead of printing them out also in rod_result.html'). This should enhance visibility.
-# #####		print ROD "<td><a href=\"$url\" target=_blank>$pos</a></td>";
-# ### Information about the CDS
-
-# 		if (!(ref($seqio_obj) =~ m/\:\:fasta$/i)) { # see above
-# 		    my $loop = 0;
-# 		    my $counter;
-# 		    foreach my $feature (@{$features{$acc}}) {
-# 			if ($feature->location->start >= $start && $feature->location->end <= $end) { #####what about features that overlap start and stop coordinates?!
-# 			    if ($feature->primary_tag eq 'CDS') {
-# 				if ($loop >= 1) {
-# 				    print ROD "<tr>", "<td></td><td></td><td></td>";
-# 				}
-# 				print ROD "<td><b>", $feature->get_tag_values('product'), "</b></td>";
-# 				print ROD "<td>", $feature->location->start, ":", $feature->location->end, "</td>";
-# 				print ROD "<td>", $feature->get_tag_values('translation'), "</td>";
-# 				print ROD "</tr>\n";
-# 				print "Counter inside: ", $counter++, " ", $feature->get_tag_values('product'), ", loop: $loop\n";
-# 				$loop = 1;
-# 			    }
-# 			} elsif ($feature->location->start <= $start && ($feature->location->end > $start && $feature->location->end <= $end)) {
-# 			    if ($feature->primary_tag eq 'CDS') {
-# 				if ($loop >= 1) {
-# 				    print ROD "<tr>", "<td></td><td></td><td></td>";
-# 				}
-# 				print ROD "<td><b>", $feature->get_tag_values('product'), "</b> (overlap)</td>";
-# 				print ROD "<td>", $feature->location->start, ":", $feature->location->end, "</td>";
-# 				print ROD "<td>", $feature->get_tag_values('translation'), "</td>";
-# 				print ROD "</tr>\n";
-# 				print "Counter end-overlap: ", $counter++, " ", $feature->get_tag_values('product'), ", loop: $loop\n";
-# 				$loop = 1;
-# 			    }
-# 			} elsif (($feature->location->start >= $start && $feature->location->start < $end) && $feature->location->end > $end) {
-# 			    if ($feature->primary_tag eq 'CDS') {
-# 				if ($loop >= 1) {
-# 				    print ROD "<tr>", "<td></td><td></td><td></td>";
-# 				}
-# 				print ROD "<td><b>", $feature->get_tag_values('product'), "</b> (overlap)</td>";
-# 				print ROD "<td>", $feature->location->start, ":", $feature->location->end, "</td>";
-# 				print ROD "<td>", $feature->get_tag_values('translation'), "</td>";
-# 				print ROD "</tr>\n";
-# 				print "Counter start-overlap: ", $counter++, " ", $feature->get_tag_values('product'), ", loop: $loop\n";
-# 				$loop = 1;
-# 			    }
-# 			}
-# 		    }
-# 		}
-# 	    }
-# 	}
-#     }
-# }
-# print ROD "\n</table>\n";
-# print ROD $cgi->end_html;
-# close ROD;
-
-
-### Print which files have been created! --> see get_Ecoli_MLST_alleles.pl
-print "\nThe following files were created:\n";
-print "- rod_result_summary.txt: Summary of the found regions of difference (RODs)\n";
-print "- rod_result.gff: GFF3 file with ROD coordinates to use in artemis/DNAPlotter ...\n";
-print "- rod_seq.fasta: Fasta sequences of the ROD regions\n";
+### Print which files have been created!
+print "\n\nThe following files were created:\n";
+print "- $rod_result: Summary of the found regions of difference (RODs)\n";
+print "- $gff_out: GFF3 file with ROD coordinates to use in artemis/DNAPlotter ...\n";
+print "- $brig_out: Tab-seperated file for BRIG (blast ring image generator)\n";
+print "- $seq_out: Fasta sequences of the ROD regions\n";
 if (!(ref($seqio_obj) =~ m/\:\:fasta$/i)) {
-    print "- rod_aa_fasta.txt: Fasta amino acid sequences of CDSs in ROD regions\n";
+    print "- $cds_out: Fasta amino acid sequences of CDSs in ROD regions\n";
 }
 print "\n";
+
 
 exit;
 
@@ -310,20 +230,39 @@ sub print_CDSs {
     my $feature = shift;
     my $loop = shift;
     my $overlap = shift;
-    if ($feature->primary_tag eq 'CDS') {
+    my $primary_tag = $feature->primary_tag; # variable just to make the code shorter
+    if (($primary_tag eq 'CDS') || ($primary_tag eq 'tRNA') || ($primary_tag eq 'rRNA') || ($primary_tag eq 'tmRNA') || ($primary_tag eq 'ncRNA') || ($primary_tag eq 'repeat_region') || ($primary_tag eq 'misc_RNA') || ($primary_tag eq 'misc_binding')) {
 	if ($loop >= 1) { # for the features after the first line skip to the 'ROD CDSs' column
 	    print ROD "\t\t";
 	}
-	print ROD "\t", $feature->get_tag_values('product');
-	print CDS ">", $feature->get_tag_values('product');
-	if (defined $overlap) {
-	    print ROD " (overlap)";
-	    print CDS " (overlap)\n";
+	if ($feature->has_tag('product')) { # TRUE if tag 'product' exists for this feature, otherwise error is thrown
+	    print ROD "\t", $feature->get_tag_values('product');
+	    print CDS ">", $feature->get_tag_values('product');
+	} elsif ($feature->has_tag('note')) {
+	    print ROD "\t", $feature->get_tag_values('note');
+	    print CDS ">", $feature->get_tag_values('note');
+	} elsif ($feature->has_tag('rpt_family')) {
+	    print ROD "\t", $feature->get_tag_values('rpt_family');
+	    print CDS ">", $feature->get_tag_values('rpt_family');
 	}
-	print ROD "\t", $feature->location->start, "..", $feature->location->end, "\n";
-	print CDS $feature->get_tag_values('translation'), "\n";
+	print ROD "\t", $feature->location->start, "..", $feature->location->end;
+	if (defined $overlap) {
+	    print ROD " (overlap)\n";
+	    print CDS " (overlap)\n";
+	} else {
+	    print ROD "\n";
+	    print CDS "\n";
+	}
+	if (($primary_tag eq 'CDS') && ($feature->has_tag('translation'))) {
+	    print CDS $feature->get_tag_values('translation'), "\n"; # translation only defined for CDSs not RNAs ...
+	} else {
+	    print CDS "No translation available\n";
+	}
 	$loop++; # Indicates that more than one feature in the ROD range
 	return $loop;
+    } elsif ($primary_tag eq 'mobile_element') {
+	print ROD "\t", $feature->get_tag_values('mobile_element_type') if $feature->has_tag('mobile_element_type');
+	print CDS ">", $feature->get_tag_values('mobile_element_type') if $feature->has_tag('mobile_element_type');
     }
     return $loop;
 }
