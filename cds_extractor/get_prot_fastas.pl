a86ab2f811b32251e835c82c9aec1f7b13d461aa
aleimba <aleimba@gmx.de>
2014-01-10 10:12:59 +0100

    cds_extractor v0.4, earlier named 'get_prot_fastas'; new: replace whitespaces in values of tag /product with underscores

--- cds_extractor/get_prot_fastas.pl
+++ cds_extractor/get_prot_fastas.pl
@@ -23,7 +23,7 @@ my $usage = "\n".
    "\t# Optionally, a file with locus tags can be given to extract    #\n".
    "\t# only these proteins (each locus tag in a new line).           #\n".
    "\t#                                                               #\n".
-   "\t# version 0.3 (update 06.09.2012)              Andreas Leimbach #\n".
+   "\t# version 0.4 (update 06.02.2013)              Andreas Leimbach #\n".
    "\t# 24.05.2012                              aleimba[at]gmx[dot]de #\n".
    "\t#################################################################\n\n";
 
@@ -43,7 +43,7 @@ if (!defined $file) {
 
 
 ### Create a bioperl SeqIO object with the file
-my $seqio_object = Bio::SeqIO->new(-file => "<$file");
+my $seqio_object = Bio::SeqIO->new(-file => "<$file"); # no '-format' to leave to bioperl guessing
 
 
 ### Print the input and output file which are processed and written, resp.
@@ -55,33 +55,32 @@ open(FILE, ">$file\_prot.fasta");
 
 ### Get the protein fasta sequences from the translation tag in the embl/genbank file
 my $organism; # store the organism information of the genome file to include in each header, include plasmid names
-my $no_locus_tag = 0; # Give ONE info if no locus tag is found on a CDS
+my $no_locus_tag = 0; # print ONE error statement (i.e. only for the first) if no locus tag is found on a CDS
 while (my $seq_object = $seqio_object->next_seq) {
     my $count = 0; # for CDSs without any of the features asked for below, counts also 'pseudo' CDSs
     foreach my $feat_object ($seq_object->get_SeqFeatures) {
 	if ($feat_object->primary_tag eq 'source') {
 	    ($organism) = $feat_object->get_tag_values('organism'); # values always returned as ARRAYS!
 	    $organism =~ s/\s/\_/g;
-	    my $plasmid = eval_sub($feat_object, 'plasmid'); # for plasmid sequences
-	    if ($plasmid) {
+	    if ($feat_object->has_tag('plasmid')) { # 'eval_sub' also possible, but 'has_tag' more elegantly
+		my ($plasmid) = $feat_object->get_tag_values('plasmid');
 		$plasmid =~ s/\s/\_/g;
 		$organism = $organism.'-plasmid_'.$plasmid;
 	    }
 	}
 	if ($feat_object->primary_tag eq 'CDS') {
 	    $count++;
-	    if ($feat_object->has_tag('pseudo')) { # skip pseudogenes, the don't include /translations!
+	    if ($feat_object->has_tag('pseudo')) { # skip pseudogenes, they don't include /translations!
 		next;
 	    }
-	    if ($feat_object->has_tag('locus_tag')) { # Use a different order of id-tags here or an option which one to use (see ToDo above)?
-		if (defined $locus_list) {
+	    if ($feat_object->has_tag('locus_tag')) {
+		if (defined $locus_list) { # if locus_list is given only get those CDSs
 		    open(LOCUS, "<$locus_list");
 		    while (my $locus = <LOCUS>) {
 			chomp $locus;
 			my ($feat_locus) = $feat_object->get_tag_values('locus_tag'); # values always returned as ARRAYS!
 			if ($locus eq $feat_locus) {
-			    my $product = eval_sub($feat_object, 'product'); # subroutine to evaluate the existence of the tag and catch the error if not existent
-			    $product =~ s/\s/_/g;
+			    my $product = eval_sub($feat_object, 'product'); # subroutine to evaluate the existence of the tag and catch the error if not existent (for 'product' also replaces whitespaces with '_'); method 'has_tag' not possible, because variable $product would not be defined in print below (with 'eval_sub' the variable is just empty, but defined)
 			    my $gene = eval_sub($feat_object, 'gene');
 			    print FILE ">", $feat_object->get_tag_values('locus_tag'), " g=$gene p=$product o=$organism\n";
 			    print FILE $feat_object->get_tag_values('translation'), "\n";
@@ -90,7 +89,6 @@ while (my $seq_object = $seqio_object->next_seq) {
 		    next;
 		}
 		my $product = eval_sub($feat_object, 'product');
-		$product =~ s/\s/_/g;
 		my $gene = eval_sub($feat_object, 'gene');
 		print FILE ">", $feat_object->get_tag_values('locus_tag'), " g=$gene p=$product o=$organism\n";
 	    } elsif (defined $locus_list) { # in case a list of locus tags is given, no need to look at CDSs that doesn't have a locus tag
@@ -99,7 +97,6 @@ while (my $seq_object = $seqio_object->next_seq) {
 		my ($protein_id) = $feat_object->get_tag_values('protein_id');
 		locus_tag_info($no_locus_tag, $organism, $protein_id); # subroutine to inform no locus tag is found
 		my $product = eval_sub($feat_object, 'product');
-		$product =~ s/\s/_/g;
 		my $gene = eval_sub($feat_object, 'gene');
 		print FILE ">$protein_id g=$gene p=$product o=$organism\n";
 		$no_locus_tag++;
@@ -107,13 +104,11 @@ while (my $seq_object = $seqio_object->next_seq) {
 		my ($gene) = $feat_object->get_tag_values('gene');
 		locus_tag_info($no_locus_tag, $organism, $gene);
 		my $product = eval_sub($feat_object, 'product');
-		$product =~ s/\s/_/g;
 		print FILE ">$gene p=$product o=$organism\n";
 		$no_locus_tag++;
 	    } elsif  ($feat_object->has_tag('product')) {
 		my ($product) = $feat_object->get_tag_values('product');
 		locus_tag_info($no_locus_tag, $organism, $product);
-		$product =~ s/\s/_/g;
 		print FILE ">$product o=$organism\n";
 		$no_locus_tag++;
 	    } else { # if none of the above tags are existent use the internal counter
@@ -145,6 +140,9 @@ sub eval_sub {
     my ($feat_object, $tag) = @_;
     my $value = '';
     eval {($value) = $feat_object->get_tag_values($tag);}; # catch error if tag doesn't exist
+    if ($tag eq 'product') {
+	$value =~ s/\s/_/g;
+    }
     return $value;
 }
 
