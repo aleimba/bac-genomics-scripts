56de53ef03ebd21005c20be6f3c306fde421dde7
aleimba <aleimba@gmx.de>
2014-01-12 21:07:03 +0100

    cds_extractor v0.6; forgot to overwrite previous version

--- cds_extractor/cds_extractor.pl
+++ cds_extractor/cds_extractor.pl
@@ -12,13 +12,13 @@ cds_extractor.pl                                           24-05-2012
 
 =head1 SYNOPSIS
 
-C<perl cds_extractor.pl -s seq_file.gbk -p>
+C<perl cds_extractor.pl -i seq_file.embl -p>
 
 =head1 DESCRIPTION
 
-Extracts protein or DNA sequences from CDS features from a
+Extracts protein or DNA sequences of CDS features from a
 (multi)-embl or -genbankfile and writes them to a multi-fasta file.
-The fasta ID line includes either the locus tag (plus g=gene
+The fasta ID line includes either the locus tag (plus g=gene,
 p=product, o=organism; if existent), if that's not available protein
 id (plus g=gene, p=product, o=organism; if existent), gene (plus
 p=product, o=organism), product (plus o=organism), or an internal
@@ -26,15 +26,12 @@ CDS counter (in this order). The organism info includes also
 possible plasmid names. Pseudogenes (tagged by /pseudo) are not
 included (except in the CDS counter).
 
-CDS without locus tags are written to the error file
+CDSs without locus tags are written to an error file
 I<locus_tag_errors.txt> (if the file already exists, errors will
 be appended to it)!
 
 Optionally, a file with locus tags can be given to extract only
 these proteins (each locus tag in a new line).
-
-WARNING: Non-pseudo CDS features with a join (i.e. including a
-frameshift) lead to wrong nucleotide sequences.
 
 The Perl script runs on BioPerl (L<http://www.bioperl.org>).
 
@@ -44,9 +41,9 @@ The Perl script runs on BioPerl (L<http://www.bioperl.org>).
 
 =over 23
 
-=item B<-s>=I<str>, B<-seq_file>=I<str>
+=item B<-i>=I<str>, B<-input>=I<str>
 
-RichSeq sequence file including annotation (embl or genbank)
+Input RichSeq sequence file including annotation (embl or genbank)
 
 =item B<-p>, B<-protein>
 
@@ -103,7 +100,7 @@ Multi-fasta file of CDS DNA sequences
 
 =item (F<locus_tag_errors.txt>)
 
-Indicates CDS features that don't have a locus tag
+Lists CDS features without locus tags
 
 =back
 
@@ -111,19 +108,19 @@ Indicates CDS features that don't have a locus tag
 
 =over
 
-=item C<perl cds_extractor.pl -s seq_file.gbk -p -l
+=item C<perl cds_extractor.pl -i seq_file.gbk -p -l
 locus_tags.txt>
 
-=item C<perl cds_extractor.pl -s seq_file.gbk -n -l
+=item C<perl cds_extractor.pl -i seq_file.embl -n -l
 locus_tags.txt -u 100 -d 20>
 
-=item C<perl cds_extractor.pl -s seq_file.gbk -p -f>
+=item C<perl cds_extractor.pl -i seq_file.gbk -p -f>
 
 =back
 
 =head1 VERSION
 
-0.5                                                update: 03-06-2013
+0.6                                                update: 06-06-2013
 
 =head1 AUTHOR
 
@@ -160,7 +157,7 @@ use Bio::SeqFeatureI; # bioperl module to handle features in a sequence
 
 
 
-### Get the options with Getopt::Long, works also abbreviated and with two "--": -s, --s, -seq_file ...
+### Get the options with Getopt::Long, works also abbreviated and with two "--": -i, --i, -input ...
 my $seq_file = ''; # RichSeq sequence file including feature annotation
 my $protein = ''; # extract protein sequences for each CDS feature; excludes option '-n'
 my $nucleotide = ''; # extract nucleotide sequences for each CDS feature; excludes option '-p'
@@ -169,7 +166,7 @@ my $downstream = 0; # include given number of flanking nucleotides downstream of
 my $locustag_list; # list of locus tags to extract only those
 my $full_header; # include a full ID header for downstream 'blast_prot_finder.pl' analysis (needed for regex in subroutine 'split_ID')
 my $help = ''; # run perldoc on POD
-GetOptions ('seq_file=s' => \$seq_file, 'protein' => \$protein, 'nucleotide' => \$nucleotide, 'upstream:i' => \$upstream, 'downstream:i' => \$downstream, 'locustag_list:s' => \$locustag_list, 'full_header' => \$full_header, 'help' => \$help);
+GetOptions ('input=s' => \$seq_file, 'protein' => \$protein, 'nucleotide' => \$nucleotide, 'upstream:i' => \$upstream, 'downstream:i' => \$downstream, 'locustag_list:s' => \$locustag_list, 'full_header' => \$full_header, 'help' => \$help);
 
 
 
@@ -202,13 +199,29 @@ my $seqio_object = Bio::SeqIO->new(-file => "<$seq_file"); # no '-format' to lea
 
 ### Print the input and output file which are processed and written, respectively
 print "\nInput: $seq_file\t";
+my $ori_filename = $seq_file; # store original filename to give error statements
 $seq_file =~ s/(.+)\.\w+$/$1/;
 if ($protein) {
-    print "Output: $seq_file\_cds_aa.fasta\n";
-    open (OUT, ">$seq_file\_cds_aa.fasta") or die "Failed to create file \'$seq_file\_cds_aa.fasta\': $!\n";
+    $seq_file = $seq_file.'_cds_aa.fasta';
+    print "Output: $seq_file\n";
+    open (OUT, ">$seq_file") or die "Failed to create file \'$seq_file\': $!\n";
 } elsif ($nucleotide) {
-    print "Output: $seq_file\_cds_nucl.fasta\n";
-    open (OUT, ">$seq_file\_cds_nucl.fasta") or die "Failed to create file \'$seq_file\_cds_nucl.fasta\': $!\n";
+    $seq_file = $seq_file.'_cds_nucl.fasta';
+    print "Output: $seq_file\n";
+    open (OUT, ">$seq_file") or die "Failed to create file \'$seq_file\': $!\n";
+}
+
+
+
+### Get the list of locus tags if $locustag_list defined
+my %locus_tags; # store locus tags and indicate if present in seq_file
+if (defined $locustag_list) {
+    open (LOCUS, "<$locustag_list") or die "Failed to open file \'$locustag_list\': $!\n";
+    while (my $locus = <LOCUS>) {
+	chomp $locus;
+	$locus_tags{$locus} = 0; # changes to 1 if the locus tag was found in seq_file
+    }
+    close LOCUS;
 }
 
 
@@ -217,15 +230,20 @@ if ($protein) {
 my $organism; # store the organism information of the genome file to include in each header, include plasmid names
 my $no_locus_tag = 0; # print ONE error statement (i.e. only for the first) if no locus tag is found on a CDS
 my %double_id; # control if a locus tag or protein id is double in the file, then exit (they should be unambiguous); see sub 'control_double'
-my %missed_locus; # print message if locus tags in '$locustag_list' were not found
 while (my $seq_object = $seqio_object->next_seq) {
     my $count = sprintf("%04d", 0); # for CDSs without any of the features asked for below, counts also 'pseudo' CDSs
-    foreach my $feat_object ($seq_object->get_SeqFeatures) {
+    my @feat_objects = $seq_object->get_SeqFeatures; # slurp all features from the seq to check CDS tag annotation
+    if (grep ($_->primary_tag eq 'CDS', @feat_objects) == 0) {
+	close OUT;
+	unlink $seq_file or die "Couldn't remove empty result file \'$seq_file\': $!\n";
+	die "\n###Fatal error\nSequence with no CDS annotation in \'$ori_filename\'!\nExiting program!\n\n";
+    }
+    foreach my $feat_object (@feat_objects) { # a Bio::SeqFeatureI object
 	if ($feat_object->primary_tag eq 'source') {
 	    $organism = eval_sub($feat_object, 'organism');
 	    $organism =~ s/\s/\_/g;
 	    if ($feat_object->has_tag('plasmid')) { # 'eval_sub' also possible, but 'has_tag' more elegantly
-		my ($plasmid) = $feat_object->get_tag_values('plasmid');
+		my ($plasmid) = $feat_object->get_tag_values('plasmid'); # values always returned as ARRAYS; might use 'join(' ', $feat_object->get_tag_values($tag))' instead
 		$plasmid =~ s/\s/\_/g;
 		$organism = $organism.'-plasmid_'.$plasmid;
 	    }
@@ -238,29 +256,26 @@ while (my $seq_object = $seqio_object->next_seq) {
 	    if ($feat_object->has_tag('locus_tag')) {
 		control_double($feat_object->get_tag_values('locus_tag'), 'Locus tag'); # subroutine to control if locus tag already exists
 		if (defined $locustag_list) { # if locus_list is given only get those CDSs
-		    open (LOCUS, "<$locustag_list") or die "Failed to open file \'$locustag_list\': $!\n";
-		    while (my $locus = <LOCUS>) {
-			chomp $locus;
-			if (!$missed_locus{$locus}) {
-			    $missed_locus{$locus} = 0;
-			}
-			my ($feat_locus) = $feat_object->get_tag_values('locus_tag'); # values always returned as ARRAYS
-			if ($locus =~ /$feat_locus/) {
-			    $missed_locus{$locus} = 1;
-			    my $product = eval_sub($feat_object, 'product'); # subroutine to evaluate the existence of the tag and catch the error if not existent (for 'product' also replaces whitespaces with '_'); method '->has_tag' not possible, because variable $product would not be defined in print below (with 'eval_sub' the variable is just empty, but defined)
-			    my $gene = eval_sub($feat_object, 'gene');
-			    print OUT ">$feat_locus g=$gene p=$product o=$organism\n";
-			    print_seq($feat_object, $seq_object); # subroutine to print the protein or nucleic sequence
-			    last; # exit while loop if locus_tag was found
-			}
+		    if (grep ($locus_tags{$_} == 1, keys %locus_tags) == keys %locus_tags) { # scalar context
+			last; # skip residual feature objects if all locus tags are found
+		    }
+		    my ($feat_locus) = $feat_object->get_tag_values('locus_tag'); # values always returned as ARRAYS
+		    my ($locus) = grep (/$feat_locus/, keys %locus_tags);
+		    if ($locus) {
+			$locus_tags{$locus} = 1;
+			my $product = eval_sub($feat_object, 'product'); # subroutine to evaluate the existence of the tag and catch the error if not existent (for 'product' also replaces whitespaces with '_'); method '->has_tag' not possible, because variable $product would not be defined in print below (with 'eval_sub' the variable is empty, but defined)
+			my $gene = eval_sub($feat_object, 'gene');
+			print OUT ">$feat_locus g=$gene p=$product o=$organism\n";
+			print_seq($feat_object, $seq_object); # subroutine to print the protein or nucleic sequence
 		    }
 		    next; # jump to the next feature object
 		}
+                ### '$locustag_list' is not defined:
 		my $product = eval_sub($feat_object, 'product');
 		my $gene = eval_sub($feat_object, 'gene');
 		print OUT ">", $feat_object->get_tag_values('locus_tag'), " g=$gene p=$product o=$organism\n";
 	    } elsif (defined $locustag_list) { # in case a list of locus tags is given, no need to look at CDSs that don't have a locus tag
-		next;
+		next; # jump to the next feature object
 	    } elsif ($feat_object->has_tag('protein_id')) {
 		my ($protein_id) = $feat_object->get_tag_values('protein_id');
 		control_double($protein_id, 'Protein id');
@@ -301,6 +316,11 @@ while (my $seq_object = $seqio_object->next_seq) {
 	    print_seq($feat_object, $seq_object);
 	}
     }
+    if (defined $locustag_list) {
+	if (grep ($locus_tags{$_} == 1, keys %locus_tags) == keys %locus_tags) {
+	    last; # skip residual sequence objects if all locus tags are found
+	}
+    }
 }
 if (-e 'locus_tag_errors.txt') {
     close ERR;
@@ -309,13 +329,12 @@ close OUT;
 
 
 
-### Close $locustag_list FH and print locus tags that were not found
+### Print locus tags that were not found in seq_file
 if (defined $locustag_list) {
-    close LOCUS;
-    my @missed = sort grep ($missed_locus{$_} == 0, keys %missed_locus);
+    my @missed = grep ($locus_tags{$_} == 0, keys %locus_tags);
     if (@missed) {
-	print "### The following locus tags were not found in \'$seq_file\':\n";
-	foreach (@missed) {
+	print "### The following locus tags were not found in \'$ori_filename\':\n";
+	foreach (sort @missed) {
 	    print "$_\t";
 	}
 	print "\n";
@@ -334,7 +353,7 @@ exit;
 sub control_double {
     my ($id, $type) = @_;
     if ($double_id{$id}) {
-	die "\n###Fatal error!\n$type \'$id\' exists at least two times in the sequence file, but should be unambiguous!\nExiting program!\n\n";
+	die "\n###Fatal error!\n$type \'$id\' exists at least two times in \'$organism\' of \'$ori_filename\', but should be unambiguous! Please modify all repetitive occurences.\nExiting program!\n\n";
     } else {
 	$double_id{$id} = 1;
     }
@@ -380,11 +399,28 @@ sub print_seq {
 	print OUT $feat_object->get_tag_values('translation'), "\n";
     } elsif ($nucleotide) {
 	if ($feat_object->strand == 1) { # feature on leading strand
-	    print OUT $seq_object->subseq($feat_object->start - $upstream, $feat_object->end + $downstream), "\n";
+	    my $start = $feat_object->start - $upstream;
+	    my $stop =  $feat_object->end + $downstream;
+	    if ($start < 0) { # 'seq_obj->subseq' doesn't work with negative numbers, but with numbers > lenghts of seq_obj
+		$start = $seq_object->length - abs($start);
+		$stop = $seq_object->length + $stop;
+	    }
+	    my $subseq = $seq_object->subseq($start, $stop);
+	    print OUT substr($subseq, 0, $upstream); # upstream bases, not possible to include in '$feat_obj->spliced_seq'
+	    print OUT $feat_object->spliced_seq->seq; # to include also features with "join" locations; '$feat_object->spliced_seq' returns a Bio::Seq object thus call funtion '->seq' for the seq string
+	    print OUT substr($subseq, length($subseq) - $downstream, $downstream), "\n"; # downstream bases
 	} elsif ($feat_object->strand == -1) { # feature on lagging strand
-	    my $trunc_obj = $seq_object->trunc($feat_object->start - $downstream, $feat_object->end + $upstream); # a Bio::Seq object
+	    my $start = $feat_object->start - $downstream;
+	    my $stop = $feat_object->end + $upstream;
+	    if ($start < 0) {
+		$start = $seq_object->length - abs($start);
+		$stop = $seq_object->length + $stop;
+	    }
+	    my $trunc_obj = $seq_object->trunc($start, $stop); # a Bio::Seq object, needed for revcom
 	    my $rev_obj = $trunc_obj->revcom; # a Bio::Seq object
-	    print OUT $rev_obj->seq(), "\n";
+	    print OUT substr($rev_obj->seq, 0, $upstream);
+	    print OUT $feat_object->spliced_seq->seq; # to include also features with "join" locations
+	    print OUT substr($rev_obj->seq, $rev_obj->length - $downstream, $downstream), "\n";
 	}
     }
     return 1;
