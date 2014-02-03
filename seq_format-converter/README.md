e5d0bc051266e81844dc5ce177d3cd589a86314f
aleimba <aleimba@gmx.de>
2014-02-03 23:02:03 +0100

    seq_format-converter v0.2 plus README; short 'gbk', autodie, usage as HERE, Getopt::Long

---
+++ seq_format-converter/README.md
@@ -0,0 +1,77 @@
+seq_format-converter
+====================
+
+A script to convert a sequence file to another format.
+
+## Synopsis
+
+    perl seq_format-converter.pl -i seq_file.gbk -f gbk -o embl
+
+## Description
+
+This script converts a (multi-)sequence file of a specific format to a differently formatted output file. The most common sequence formats are: **embl**, **fasta**, and **gbk** (genbank).
+
+Since sequence formats change from time to time, BioPerl is not always up to date. For all available BioPerl sequence formats see: http://www.bioperl.org/wiki/HOWTO:SeqIO#Formats. **Warning**: The *bioperl-ext* package and the *io_lib* library from the **Staden** package (http://staden.sourceforge.net/) need to be installed in order to read the scf, abi, alf, pln, exp, ctf, ztr formats.
+
+## Usage
+
+    perl seq_format-converter.pl -i seq_file -f in_format -o out_format
+
+### UNIX loop to reformat all sequence files in the current working directory
+
+    for i in *.[embl|gbk]; do perl seq_format-converter.pl -i $i -f [embl|gbk] -o [embl|fasta|gbk]; done
+
+## Options for *seq_format-converter.pl*
+
+### Mandatory options
+
+* -i, -input
+
+Input sequence file
+
+* -f, -format
+
+Input sequence format (e.g. 'embl' or 'gbk)
+
+* -o, -out_format
+
+Output sequence format (e.g. 'embl', 'fasta' or 'gbk)
+
+### Optional options
+
+* -h, -help
+
+Print usage
+
+* -v, -version
+
+Print version number
+
+## Output
+
+* seq_file.[embl|fasta|gbk]
+
+Output sequence file in the specified format
+
+## Run environment
+
+The Perl script runs under Windows and UNIX flavors.
+
+## Dependencies (not in the core Perl modules)
+
+* BioPerl (tested with version 1.006901)
+
+## Author/contact
+
+Andreas Leimbach (aleimba[at]gmx[dot]de; Microbial Genome Plasticity, Institute of Hygiene, University of Muenster)
+
+## Changelog
+
+* v0.2 (03.02.2014)
+    - allow short 'gbk' format instead of 'genbank'
+    - also short 'gbk' file-extension for output file
+    - included 'use autodie'
+    - usage as HERE document
+    - options with Getopt::Long
+    - version switch
+* v0.1 (10.11.2011)
--- seq_format-converter/seq_format-converter.pl
+++ seq_format-converter/seq_format-converter.pl
@@ -2,46 +2,79 @@
 
 use warnings;
 use strict;
+use autodie;
+use Getopt::Long;
 use Bio::SeqIO; # bioperl module to handle sequence input/output
 
-my $usage = "\n".
-   "\t####################################################################\n".
-   "\t# $0 infile infileformat outfileformat        #\n". #$0 = program name
-   "\t#                                                                  #\n".
-   "\t# Converts a (multi-)sequence file of a specific format to a       #\n".
-   "\t# differently formatted output file, with the help of bioperl.     #\n".
-   "\t# www.bioperl.org                                                  #\n".
-   "\t# formats e.g.: embl, exp, fasta, genbank                          #\n".
-   "\t#                                                                  #\n".
-   "\t# Adjust unix loop to run seq_format_converter.pl with several     #\n".
-   "\t# files:                                                           #\n".
-   "\t# for i in *.gbk; do perl seq_format_converter.pl \$i genbank embl; #\n".
-   "\t# done                                                             #\n".
-   "\t#                                                                  #\n".
-   "\t# version 0.1                                           A Leimbach #\n".
-   "\t# 10.11.2011                                 aleimba[at]gmx[dot]de #\n".
-   "\t####################################################################\n\n";
+my $usage = << "USAGE";
 
-# shift arguments from @ARGV
-my $infile = shift or die $usage;
-my $infileformat = shift or die $usage;
-my $outfileformat = shift or die $usage;
-# print usage if -h|--h|--help is given as argument
-if ($infile =~ m/-h/) {
+  ##################################################################
+  # $0 -i seq_file -f in_format -o out_format #
+  #                                                                #
+  # Converts a (multi-)sequence file of a specific format to a     #
+  # differently formatted output file, with the help of BioPerl    #
+  # (www.bioperl.org).                                             #
+  # Formats are e.g. embl, fasta, gbk.                             #
+  #                                                                #
+  # Mandatory options:                                             #
+  # -i, -input       input sequence file                           #
+  # -f, -format      input format                                  #
+  # -o, -out_format  output format                                 #
+  # Optional options:                                              #
+  # -h, -help        print usage                                   #
+  # -v, -version     print version number                          #
+  #                                                                #
+  # Adjust unix loop to run the script with all files in the       #
+  # current working directory, e.g.:                               #
+  # for i in *.gbk; do perl seq_format_converter.pl -i \$i -f gbk \\ #
+  # -o embl; done                                                  #
+  #                                                                #
+  # version 0.2, update: 03-02-2014                     A Leimbach #
+  # 10-11-2011                               aleimba[at]gmx[dot]de #
+  ##################################################################
+
+USAGE
+;
+
+### Get options with Getopt::Long
+my $infile; # input sequence file
+my $in_format; # input sequence file format
+my $out_format; # desired output file format
+my $version = 0.2;
+my ($opt_version, $opt_help);
+GetOptions ('input=s' => \$infile,
+            'format=s' => \$in_format,
+            'out_format=s' => \$out_format,
+            'version' => \$opt_version,
+            'help|?' => \$opt_help);
+
+
+### Print usage
+if ($opt_help) {
     die $usage;
+} elsif ($opt_version) {
+    die "$0 $version\n";
+} elsif (!$infile || !$in_format || !$out_format) {
+    die $usage, "### Fatal error: Option(s) or argument(s) for \'-i\', \'-f\', \'-o\' are missing!\n\n";
 }
 
-# SeqIO objects for input and output
-my $seq_in = Bio::SeqIO->new(-file => "<$infile", -format => $infileformat); # a Bio::SeqIO object
-my $outfile = $infile;
-$outfile =~ s/\..*$//; # delete extension from infile name
-#print "infile: $infile\toutfile: $outfile\t\$&: $&\n";
-my $seq_out = Bio::SeqIO->new(-file => ">$outfile.$outfileformat", -format => $outfileformat); # a Bio::SeqIO object
 
-# write sequence to different format
+### Allow shorter format string for 'genbank'
+$in_format = 'genbank' if ($in_format =~ /gbk/i);
+my $outfile = $infile;
+$outfile =~ s/\.\w+$/\.$out_format/; # remove file extension from infile and append out_format
+$out_format = 'genbank' if ($out_format =~ /gbk/i);
+
+
+### SeqIO objects for input and output
+my $seq_in = Bio::SeqIO->new(-file => "<$infile", -format => $in_format); # a Bio::SeqIO object
+my $seq_out = Bio::SeqIO->new(-file => ">$outfile", -format => $out_format); # a Bio::SeqIO object
+
+
+### Write sequence to different format
 while (my $seqobj = $seq_in->next_seq) { # a Bio::Seq object
     $seq_out->write_seq($seqobj);
 }
+print "\n\tCreated new file $outfile!\n\n";
 
-print "\n\tCreated new file $outfile.$outfileformat!\n\n";
 exit;
