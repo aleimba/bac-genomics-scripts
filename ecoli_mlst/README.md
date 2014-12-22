4b110686d51c6a331fd7e424e5dba11d672355e2
aleimba <aleimba@gmx.de>
2014-12-22 16:16:45 +0100

    ecoli_mlst v0.3
    changed script name to 'ecoli_mlst.pl', check if result files exist, additional info in POD, included allele files and 'publicSTs.txt' in repository

---
+++ ecoli_mlst/README.md
@@ -0,0 +1,134 @@
+ecoli_mlst
+==========
+
+`ecoli_mlst` is a script to determine MLST sequence types for *E. coli* genomes and extract allele sequences.
+
+* [Synopsis](#synopsis)
+* [Description](#description)
+* [Usage](#usage)
+* [Options](#options)
+  * [Mandatory options](#mandatory-options)
+  * [Optional options](#optional-options)
+* [Output](#output)
+* [Run environment](#run-environment)
+* [Author - contact](#author---contact)
+* [Changelog](#changelog)
+
+# Synopsis
+
+    perl ecoli_mlst.pl -a fas -g fasta
+
+# Description
+
+The script searches for multilocus sequence type (MLST) alleles in *E. coli* genomes according to
+Mark Achtman's scheme with seven house-keeping genes (*adk*, *fumC*, *gyrB*,
+*icd*, *mdh*, *purA*, and *recA*) [Wirth et al., 2006]. *NUCmer* from the
+[*MUMmer package*](http://mummer.sourceforge.net/) is used to compare the given allele
+sequences to bacterial genomes via nucleotide alignments.
+
+Download the allele files (adk.fas ...) and the sequence type file
+('publicSTs.txt') from this website:
+    http://mlst.ucc.ie/mlst/dbs/Ecoli
+
+To run `ecoli_mlst.pl` include all *E. coli* genome files (file
+extension e.g. 'fasta'), all allele sequence files (file extension
+'fas') and 'publicSTs.txt' in the current working directory. The
+allele profiles are parsed from the created \*.coord files and written
+to a result file, plus additional information from the file
+'publicSTs.txt'. Also, the corresponding allele sequences (obtained
+from the allele input files) are concatenated for each *E. coli* genome
+into a result multi-fasta file. Option **-c** can be used to initiate
+an alignment for this multi-fasta file with [*ClustalW*](http://www.clustal.org/clustal2/) (standard
+alignment parameters; has to be in the `$PATH` or change variable
+`$clustal_call`). The alignment fasta output file can be used
+directly for [*RAxML*](http://sco.h-its.org/exelixis/web/software/raxml/index.html). CAREFUL the Phylip alignment format from
+*ClustalW* allows only 10 characters per strain ID.
+
+`ecoli_mlst.pl` works with complete and draft genomes. However, several genomes cannot be included in a single input file!
+
+Obviously, only for those genomes whose allele sequences have been
+deposited in Achtman's allele database results can be obtained. If an
+allele is not found in a genome it is marked by a '?' in the result
+profile file and a place holder 'XXX' in the result fasta file. For
+these cases a manual *NUCmer* or *BLASTN* might be useful to fill the
+gaps and [`run_sub_seq.pl`](/run_sub_seq) to get the corresponding 'new' allele
+sequences.
+
+Non-NCBI fasta headers for the genome files have to have a
+unique ID directly following the '>' (e.g. 'Sakai', '55989' ...).
+
+# Usage
+
+    perl ecoli_mlst.pl -a fas -g fasta -c
+
+# Options
+
+## Mandatory options
+
+- -a, -alleles
+
+    File extension of the MLST allele fasta files, e.g. 'fas' (<=> **-g**).
+
+- -g, -genomes
+
+    File extension of the *E. coli* genome fasta files, e.g. 'fasta' (<=> **-a**).
+
+## Optional options
+
+- -h, -help
+
+    Help (perldoc POD)
+
+- -c, -clustalw
+
+    Call [*ClustalW*](http://www.clustal.org/clustal2/) for alignment
+
+# Output
+
+- ecoli_mlst_profile.txt
+
+    Tab-separated allele profiles for the *E. coli* genomes, plus additional info from 'publicSTs.txt'
+
+- ecoli_mlst_seq.fasta
+
+    Multi-fasta file of all concatenated allele sequences for each genome
+
+- *.coord
+
+    Text files that contain the coordinates of the *NUCmer* hits for each genome and allele
+
+- (errors.txt)
+
+    Error file, summarizing number of not found alleles or unclear *NUCmer* hits
+
+- (ecoli_mlst_seq_aln.fasta)
+
+    Optional, [*ClustalW*](http://www.clustal.org/clustal2/) alignment in Phylip format
+
+- (ecoli_mlst_seq_aln.dnd)
+
+    Optional, *ClustalW* alignment guide tree
+
+## Run environment
+
+The Perl script runs only under UNIX flavors.
+
+## Author - contact
+
+Andreas Leimbach (aleimba[at]gmx[dot]de; Microbial Genome Plasticity, Institute of Hygiene, University of Muenster)
+
+## Changelog
+
+* v0.3 (30.01.2013)
+    - additional info in POD
+    - check if result files already exist and ask user what to do
+    - changed script name from `ecoli_mlst_alleles.pl` to `ecoli_mlst.pl`
+* v0.2 (20.10.2012)
+    - included a POD
+    - options with Getopt::Long
+    - don't consider input *E. coli* genome query files, which are too big (set cutoff at 9 MB for a fasta *E. coli* file)
+    - draft *E. coli* genomes can now be used as input query files
+    - additional info in 'publicSTs.txt' now associated to found ST types in output
+    - give text to STDOUT which files were created
+    - new option **-c** to align the resulting allele sequences via *ClustalW*
+* v0.1 (25.10.2011)
--- ecoli_mlst/ecoli_mlst.pl
+++ ecoli_mlst/ecoli_mlst.pl
@@ -8,60 +8,82 @@
 
 =head1 NAME
 
-ecoli_mlst_alleles.pl                                   25-10-2011
+ecoli_mlst.pl                                       25-10-2011
 
 =head1 SYNOPSIS
 
-The script searches for MLST alleles in E. coli genomes according to
-Mark Achtman's scheme with seven house-keeping genes (adk, fumC, gyrB,
-icd, mdh, purA, and recA) [Wirth et al., 2006]. NUCmer from the MUMmer
-package (http://mummer.sourceforge.net/) is used to compare the given
-allele sequences to bacterial genomes via nucleotide alignments. The
-allele profiles are parsed from the created *.coord files and written
-to a result file, plus additional information from the file
-'publicSTs.txt'. Also, the corresponding allele sequences (obtained
-from the allele input files) are concatenated for each E. coli genome
-into a multi-fasta file. Option B<--c> can be used to initiate an
-alignment for this multi-fasta file with ClustalW (standard alignment
-parameters; has to be in the C<$PATH> or change variable
-C<$clustal_call>). The alignment output file can be used directly for
-I<RAxML>.
+C<perl ecoli_mlst.pl -a fas -g fasta>
 
-I<ecoli_mlst_alleles.pl> works with draft and complete genomes,
-however only for those genomes whose allele sequences have been
-deposited in Achtman's allele database. If the alleles cannot be found
-use I<run_sub_seq.pl> to get the corresponding 'new' allele sequences.
+=head1 DESCRIPTION
 
-Alleles that were not found in a genome are marked by a '?' in the
-result profile file and a place holder 'XXX' in the result fasta file.
+The script searches for multilocus sequence type (MLST) alleles in
+I<E. coli> genomes according to Mark Achtman's scheme with seven
+house-keeping genes (I<adk>, I<fumC>, I<gyrB>, I<icd>, I<mdh>,
+I<purA>, and I<recA>) [Wirth et al., 2006]. I<NUCmer> from the
+L<I<MUMmer package>|http://mummer.sourceforge.net/> is used to
+compare the given allele sequences to bacterial genomes via
+nucleotide alignments.
 
 Download the allele files (adk.fas ...) and the sequence type file
 ('publicSTs.txt') from this website:
-                  http://mlst.ucc.ie/mlst/dbs/Ecoli
+                  http://mlst.warwick.ac.uk/mlst/dbs/Ecoli
 
-All files have to be deposited in the folder in which the script
-is run. Non-NCBI fasta headers for the genome files have to have a
-unique ID directly following the '>'.
+To run C<ecoli_mlst.pl> include all I<E. coli> genome files (file
+extension e.g. 'fasta'), all allele sequence files (file extension
+'fas') and 'publicSTs.txt' in the current working directory. The
+allele profiles are parsed from the created *.coord files and written
+to a result file, plus additional information from the file
+'publicSTs.txt'. Also, the corresponding allele sequences (obtained
+from the allele input files) are concatenated for each I<E. coli>
+genome into a result multi-fasta file. Option B<-c> can be used to
+initiate an alignment for this multi-fasta file with
+L<I<ClustalW>|http://www.clustal.org/clustal2/> (standard alignment
+parameters; has to be in the C<$PATH> or change variable
+C<$clustal_call>). The alignment fasta output file can be used
+directly for L<I<RAxML>|http://sco.h-its.org/exelixis/web/software/raxml/index.html>. CAREFUL the Phylip alignment format from
+I<ClustalW> allows only 10 characters per strain ID.
+
+C<ecoli_mlst.pl> works with complete and draft genomes. However,
+several genomes cannot be included in a single input file!
+
+Obviously, only for those genomes whose allele sequences have been
+deposited in Achtman's allele database results can be obtained. If an
+allele is not found in a genome it is marked by a '?' in the result
+profile file and a place holder 'XXX' in the result fasta file. For
+these cases a manual I<NUCmer> or I<BLASTN> might be useful to fill the
+gaps and L<C<run_sub_seq.pl>|/run_sub_seq> to get the corresponding
+'new' allele sequences.
+
+Non-NCBI fasta headers for the genome files have to have a unique ID
+directly following the '>' (e.g. 'Sakai', '55989' ...).
 
 =head1 OPTIONS
 
 =head2 Mandatory options
 
-=over
+=over 22
 
-=item B<-a>, B<-alleles>     File extension of the MLST allele fasta files, e.g. 'fas'.
+=item B<-a>=I<str>, B<-alleles>=I<str>
 
-=item B<-g>, B<-genomes>     File extension of the E. coli genome fasta files, e.g. 'fasta'.
+File extension of the MLST allele fasta files, e.g. 'fas' (<=> B<-g>).
+
+=item B<-g>=I<str>, B<-genomes>=I<str>
+
+File extension of the I<E. coli> genome fasta files, e.g. 'fasta' (<=> B<-a>).
 
 =back
 
-=head2 Optional option
+=head2 Optional options
 
 =over
 
-=item B<--h>, B<--help>      Help (perldoc POD).
+=item B<-h>, B<-help>
 
-=item B<--c>, B<--clustalw>  Call ClustalW for alignment.
+Help (perldoc POD)
+
+=item B<-c>, B<-clustalw>
+
+Call L<I<ClustalW>|http://www.clustal.org/clustal2/> for alignment
 
 =back
 
@@ -69,21 +91,29 @@ unique ID directly following the '>'.
 
 =over 17
 
-=item F<Ecoli_MLST_profile.txt>
+=item F<ecoli_mlst_profile.txt>
 
-Tab-separated allele profiles for the E. coli genomes, plus additional info from 'publicSTs.txt'.
+Tab-separated allele profiles for the I<E. coli> genomes, plus additional info from 'publicSTs.txt'
 
-=item F<Ecoli_MLST_seq.fasta>
+=item F<ecoli_mlst_seq.fasta>
 
-Multi-fasta file of all concatenated allele sequences for each genome.
+Multi-fasta file of all concatenated allele sequences for each genome
 
 =item F<*.coord>
 
-Text files that contain the coordinates of the NUCmer hits for each genome and allele.
+Text files that contain the coordinates of the I<NUCmer> hits for each genome and allele
 
 =item (F<errors.txt>)
 
-Error file, summarizing number of not found alleles or unclear NUCmer hits.
+Error file, summarizing number of not found alleles or unclear I<NUCmer> hits
+
+=item (F<ecoli_mlst_seq_aln.fasta>)
+
+Optional, L<I<ClustalW>|http://www.clustal.org/clustal2/> alignment in Phylip format
+
+=item (F<ecoli_mlst_seq_aln.dnd>)
+
+Optional, I<ClustalW> alignment guide tree
 
 =back
 
@@ -91,13 +121,13 @@ Error file, summarizing number of not found alleles or unclear NUCmer hits.
 
 =over
 
-=item C<perl ecoli_mlst_alleles.pl -a fas -g fasta [-c]>
+=item C<perl ecoli_mlst.pl -a fas -g fasta -c>
 
 =back
 
 =head1 VERSION
 
-0.2                                                update: 20-10-2012
+0.3                                                update: 30-01-2013
 
 =head1 AUTHOR
 
@@ -105,7 +135,7 @@ Andreas Leimbach                                aleimba[at]gmx[dot]de
 
 =head1 LICENSE
 
-This program is free software; you can redistribute it and/or modify
+This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 (GPLv3) of the License,
 or (at your option) any later version.
@@ -116,9 +146,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
-USA.
+along with this program. If not, see L<http://www.gnu.org/licenses/>.
 
 =cut
 
@@ -132,16 +160,11 @@ use warnings;
 use Getopt::Long; # module to get options from the command line
 
 
-### ToDo
-# - blastn alignments to control allele hits?
-# - NUCmer/blastn also to find alleles not yet present in Achtman's DB, get the coordinates from the blast output and subsequently the alleles from the respective E. coli genomes? --> 'get_sub_seq.pl'
-
-
 ### Get the options with Getopt::Long, works also abbreviated and with two "--": -g, --g, -genomes ...
 my $usage = "perldoc $0";
 die system($usage) unless @ARGV;
-my $allele_ext = ''; # file extension of allele fasta files
-my $genome_ext = ''; # file extension of E. coli genome fasta files
+my $allele_ext = ''; # file extension of allele fasta files (<=> $genome_ext)
+my $genome_ext = ''; # file extension of E. coli genome fasta files (<=> allele_ext)
 my $clustalw = ''; # optionally, call ClustalW for alignment
 my $help = ''; # run perldoc on the POD
 GetOptions ('alleles=s' => \$allele_ext, 'genomes=s' => \$genome_ext, 'clustalw' => \$clustalw, 'help' => \$help);
@@ -155,6 +178,23 @@ if (!$genome_ext || !$allele_ext) {
 }
 
 
+### Check if result files already exist, overwrite or exit script after user question
+my $ecoli_allele_seq = 'ecoli_mlst_seq.fasta'; # multi-fasta result file with concatenated allele seqs for each E. coli genome (sequences are from the MLST allele files)
+my $clustal_aln = 'ecoli_mlst_seq_aln.fasta'; # fasta alignment file from optional clustalW alignment
+if (-e $ecoli_allele_seq) {
+    print "A previous analysis exists, overwrite the old result files [y|n]? ";
+    my $stdin = <STDIN>;
+    chomp $stdin;
+    if ($stdin =~ /n/i) {
+         die "Script abborted!\n\n";
+    } elsif ($stdin =~ /y/i) {
+         if (-e $clustal_aln) { # get rid of the optional clustalW alignment fasta-file before program run
+             unlink $clustal_aln;
+         }
+    }
+}
+
+
 ### The MLST alleles from the Achtman scheme
 my @alleles = ('adk', 'fumC', 'gyrB', 'icd', 'mdh', 'purA', 'recA');
 
@@ -165,7 +205,7 @@ my @genome_files; # array to save all the fasta genome files
 my %allele_files; # hash to save all the multi-fasta allele files
 opendir(DIR, $dirname) or die "Can't opendir $dirname: $!\n";
 while (defined(my $file = readdir(DIR))) { # go through each file in the directory
-    if ($file eq 'Ecoli_MLST_seq.fasta') { # don't use the result file from a previous analysis, the allele multi fasta file 'Ecoli_MLST_multi.fasta' (see below)!
+    if ($file eq 'ecoli_mlst_seq.fasta') { # don't use the result file from a previous analysis, the allele multi fasta file 'ecoli_mlst_multi.fasta' (see below)!
     next;
     } elsif ($file =~ /^(\S+)\.$genome_ext$/ && (-s $file < 9000000)) { # don't use files > 10 MB, E. coli fasta file shouldn't be too big (normally around 5 MB), otherwise probably wrong fasta file in folder
         push (@genome_files, $file);
@@ -181,8 +221,8 @@ die "No E. coli genome fasta-files were found!\n" unless scalar @genome_files; #
 die "No allele fasta-files were found!\n" unless scalar %allele_files; # empty hash in scalar context returns 0
 
 
-### Control for multi-fasta genome files and concatenate them to a single-fasta entry (e.g. draft genomes), concatenate these and all other E. coli genome sequence files to one multi-fasta/-genome file for the subsequent nucmer run; additionally parse and associate the E. coli accession#s/unique IDs and descriptions (strain names ...) from the genomes
-my $genome_file = 'Ecoli_multi.fasta'; # the multi-genome/-fasta file for the nucmer run, used as a temp file, will be deleted at the end of the script
+### Look for multi-fasta genome files and concatenate them to a single-fasta entry (e.g. draft genomes). Subsequently concatenate all E. coli genome sequence files to one multi-fasta/-genome file for the subsequent nucmer run. Additionally parse and associate the E. coli accession#s/unique IDs and descriptions (strain names ...) from the genomes
+my $genome_file = 'ecoli_multi.fasta'; # multi-genome/-fasta file for the nucmer run, used as a temp file, will be deleted at the end of the script
 open(GENOMES, ">$genome_file") or die "Failed to create file '$genome_file': $!\n";
 my $genome_number = 0; # used to control if lines in *.coords files correspond to the overall genome count
 my %genome_desc; # hash to associate accession#/unique ID with genome descriptions
@@ -203,7 +243,7 @@ foreach my $file (@genome_files) {
     if (!defined($new_id)) {
         die "The file '$file' only contains plasmids, program exits!\n";
     }
-    my ($acc, $desc) = acc_desc($new_id); # subroutine to fill hash %genome_desc and get acc# and genome description
+    my ($acc, $desc) = acc_desc($new_id); # subroutine to fill hash %genome_desc with acc#/unique ID and genome description
     print GENOMES ">$acc $desc; artificial genome\n"; # print the now shortened ID-line for the new single-fasta entry in the result multi-genome file
     foreach (@multi) { # print the rest of the new single-fasta file
         if (/^>/) { # skip ID lines of the old multi-fasta file
@@ -231,15 +271,15 @@ my @delete; # files to be deleted after program run
 if (-e $genome_file) {
     push (@delete, $genome_file);
 } else {
-    die "The multi-fasta E. coli genome file \'$genome_file\' could not be created: $!\n";
+    die "The multi-fasta E. coli genome file \'$genome_file\' could not be created for the NUCmer run: $!\n";
 }
 
 
 ### Parse file 'publicSTs.txt' to get additional info for each sequence type
 my $st_file = 'publicSTs.txt';
 open(ST, "<$st_file")  or die "Failed to open sequence type file '$st_file': $!\n";
-my $header = <ST>; # get rid of file header
 my %seq_type; # hash to save ST info to each allele profile
+my $header = <ST>; # get rid of file header
 while (<ST>) {
     chomp;
     my @st = split (/\t/, $_);
@@ -251,18 +291,18 @@ while (<ST>) {
 close ST;
 
 
-### Run nucmer with allele sequences as REFERENCES and the created 'Ecoli_multi.fasta' file as QUERY
+### Run nucmer with allele sequences as REFERENCES and the created 'ecoli_multi.fasta' file as QUERY
 my @created_files; # used to print out files that are created at the end of the script
 my %coord_files;
 foreach (keys %allele_files) {
-    system ("nucmer --prefix $_-all_Ecoli $allele_files{$_} $genome_file"); # system call; seperate args not possible, probably because nucmer not a system command?!
-    system ("show-coords -lT $_-all_Ecoli.delta > $_-all_Ecoli.coords"); # '-l' include seq length in output, '-T' tab-separated output
-    if (-e "$_-all_Ecoli.coords") {
-    $coord_files{$_} = "$_-all_Ecoli.coords";
-    push (@delete, "$_-all_Ecoli.delta");
-    push (@created_files, "\t\t\t$_-all_Ecoli.coords\n");
+    system ("nucmer --prefix $_-all_ecoli $allele_files{$_} $genome_file"); # system call; seperate args not possible, probably because nucmer not a system command?!
+    system ("show-coords -lT $_-all_ecoli.delta > $_-all_ecoli.coords"); # '-l' include seq length in output, '-T' tab-separated output
+    if (-e "$_-all_ecoli.coords") {
+    $coord_files{$_} = "$_-all_ecoli.coords";
+    push (@delete, "$_-all_ecoli.delta");
+    push (@created_files, "\t\t\t$_-all_ecoli.coords\n");
     } else {
-    die "Coord file '$_-all_Ecoli.coords' could not be created: $!\n";
+    die "Coord file '$_-all_ecoli.coords' could not be created: $!\n";
     }
 }
 
@@ -271,16 +311,15 @@ foreach (keys %allele_files) {
 my @summary_err; # array to store error summary for error file
 my @detail_err; # array to store the more detailed errors for error file
 my $error = 0; # switches to '1' if an error is detected
-my %strain_allele; # declare hash, that's subsequently used as anonymous 'hash in hash' in sub parse_coords to associate MLST alleles and accession#s
+my %strain_allele; # declare hash, that's subsequently used as 'hash in hash' in sub parse_coords to associate MLST alleles and accession#s
 foreach (sort keys %coord_files) {
     parse_coord($coord_files{$_}, $_); # subroutine to fill %strain_allele
 }
 
 
 ### Print the corresponding allele sequences and allele profile (plus additional info) for each E. coli genome
-my $ecoli_allele_seq = 'Ecoli_MLST_seq.fasta'; # multi-fasta result file with concatenated allele seqs for each E. coli genome (sequences are from the MLST allele files)
 open(SEQ, ">$ecoli_allele_seq") or die "Failed to create file '$ecoli_allele_seq': $!\n";
-my $ecoli_allele_profile = 'Ecoli_MLST_profile.txt'; # tab-separated result file for the allele profile of each E. coli genome plus additional info from file 'publicSTs.txt' from the Achtman MLST DB
+my $ecoli_allele_profile = 'ecoli_mlst_profile.txt'; # tab-separated result file for the allele profile of each E. coli genome plus additional info from file 'publicSTs.txt' from the Achtman MLST DB
 open(PROFILE, ">$ecoli_allele_profile") or die "Failed to create file '$ecoli_allele_profile': $!\n";
 print PROFILE "Strain"; # header for profile file
 foreach (sort @alleles) {
@@ -288,8 +327,8 @@ foreach (sort @alleles) {
 }
 print PROFILE "\tST\tST_COMPLEX\tANCESTRAL_GROUP\tSOURCE\tREFSTRAIN\n"; # info from file 'publicSTs.txt'
 foreach my $acc (sort {lc $genome_desc{$a} cmp lc $genome_desc{$b}} keys %genome_desc) { # call hash %genome_desc by keys (acc#s), but sort by values (genome desc.s) of the hash case-insensitively (all in lowercase, because Perl sorts lowercase after uppercase!)
-    print SEQ ">$genome_desc{$acc} $acc\n"; # print fasta ID line for 'Ecoli_MLST_seq.fasta'
-    print PROFILE "$genome_desc{$acc}"; # print genome desc for strain in first column for 'Ecoli_MLST_profile.txt'
+    print SEQ ">$genome_desc{$acc} $acc\n"; # print fasta ID line for 'ecoli_mlst_seq.fasta'
+    print PROFILE "$genome_desc{$acc}"; # print genome desc for strain in first column for 'ecoli_mlst_profile.txt'
     my $profile = ''; # allele profile to extract info from %seq_type
     foreach my $allele (sort keys %strain_allele) {
     if (defined $strain_allele{$allele}->{$acc}) {
@@ -318,7 +357,7 @@ foreach my $acc (sort {lc $genome_desc{$a} cmp lc $genome_desc{$b}} keys %genome
     }
     print SEQ "\n\n"; # blank line in front of each ID line
     chop $profile; # get rid of the last space, so it can be used as the key in %seq_type
-    if (defined $seq_type{$profile}) { # print ST and additional info from file 'publicST.txt' in 'Ecoli_MLST_profile.txt'
+    if (defined $seq_type{$profile}) { # print ST and additional info from file 'publicST.txt' in 'ecoli_mlst_profile.txt'
     print PROFILE "\t$seq_type{$profile}\n";
     } else {
     print PROFILE "\t?\t?\t?\t?\t?\n";
@@ -346,7 +385,7 @@ if ($error == 1) { # error(s) occurred
 }
 
 
-### Delete unneeded files, temp file 'Ecoli_multi.fasta' (see above) and the *.delta files from the NUCmer run
+### Delete unneeded files, temp file 'ecoli_multi.fasta' (see above) and the *.delta files from the NUCmer run
 foreach my $goners (@delete) {
     unlink $goners or warn "Could not remove file '$goners': $!";
 }
@@ -365,7 +404,7 @@ if ($clustalw) {
     print "\nStarting ClustalW alignment with file $ecoli_allele_seq ...";
     my $out = $ecoli_allele_seq;
     $out =~ s/\.fasta$//;
-    my $clustal_call = "clustalw -infile=$ecoli_allele_seq -outfile=$out\_aln.fasta -align -type=DNA -output=phylip -tree -newtree=$out\_aln.dnd -outputtree=phylip";
+    my $clustal_call = "clustalw -infile=$ecoli_allele_seq -outfile=$clustal_aln -align -type=DNA -output=phylip -tree -newtree=$out\_aln.dnd -outputtree=phylip";
     system ($clustal_call);
 }
 
@@ -410,7 +449,7 @@ sub parse_coord {
     while (<COORD>) {
         chomp;
         if ( /\d+\t\d+\t\d+\t\d+\t(\d+)\t(\d+)\t(\d+\.\d+)\t(\d+)\t\d+\t(\D+\d+)\t(\w*\d+\.\d)$/ ) {
-        # since the ID lines of the fastas were shortened in temp file 'Ecoli_multi.fasta', the acc#s should be the last element of each line in the coords file
+        # since the ID lines of the fastas were shortened in temp file 'ecoli_multi.fasta', the acc#s should be the last element of each line in the coords file
         # $1 = length of ref alignment, $2 = length of query alignment, $3 = identity percentage,
         # $4 = length of ref seq, $5 = allele-nr. (e.g. ADK15), $6 = accession-nr.
         $lines++; # after 'if' to skip headers
@@ -427,7 +466,7 @@ sub parse_coord {
         $discarded++;
         next;
             }
-        $strain_allele{$allele}{$6} = $5; # associate allele# with acc# and store as anonymous hash in declared hash %strain_allele
+        $strain_allele{$allele}{$6} = $5; # associate allele# with acc# and store as hash in hash in %strain_allele
     }
     }
     close COORD;
